ebedb06577a2f278fd47a0e5e5942ba0bcd9e955
NadyaGlushchenko <Nadyaglushchenko@ukr.net>
2020-12-16 10:21:12 +0200

    add commit

---
+++ build/precache-manifest.dfec7c410d5b4b1bd0f2914be406252d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e6649a10142f20287f87014c1044715",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "d55a04b47490b714afb2",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.5e8f0d1c.chunk.css"
+  },
+  {
+    "revision": "f4151cca25f84c0fd69f",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.14b0ad3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.14b0ad3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d55a04b47490b714afb2",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.9df95ac2.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
